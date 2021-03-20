9148c20b623f510d919366997b06f9f5eb5af6c4
André Soares Nascimento <andresoaresnascimento@gmail.com>
2021-03-20 19:19:00 -0300

    Criada a classe Conta.kt

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,17 +2,18 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "André"
-
     var cpf: String = "123.456.789-10"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 
 }
 
 fun main() {
     val andre = Pessoa()
 
-    println(andre.nome)
-    println(andre.cpf)
-
+    println(andre.pessoaInfo())
 
 }
